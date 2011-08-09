a2309fbb813c1662e61e0fd1c26007c6fd368ffd
Richard Lyon <richthegeek@gmail.com>
2011-08-09 23:49:38 +0100

    v0.01

---
+++ queue.coffee
@@ -0,0 +1,110 @@
+class RequestQueue
+
+	# takes no arguments.
+	constructor: () ->
+		@cbs = {complete: false, success: false, update: false, error: false}
+		@count_total = 0
+		@count_complete = 0
+		@count_error = 0
+		@count_success = 0
+		@results = []
+
+	# takes a single function
+	# fn is executed when all requests have succeeded
+	# fn recieves an array of results
+	success: (fn) ->
+		@cbs.success = fn
+
+	# internally called, executes the success fn when all requests are successful
+	succeeded: (data, name) ->
+		if @count_total == @count_success and @cbs.success
+			@cbs.success(@results)
+
+
+	# takes a single function
+	# fn is executed when all requests have completed regardless of fate
+	# fn recieves an array of results
+	complete: (fn) ->
+		@cbs.complete = fn
+
+	# internally called, executes the complete fn when all requests complete
+	completed: (data, name) ->
+		if @count_total == @count_complete and @cbs.complete
+			@cbs.complete(@results)
+
+
+	# takes a single function
+	# fn is executed each time a request completes, regardless of fate
+	# fn recieves data/text, a status count [done, total], and a "succeeded" boolean
+	update: (fn) ->
+		@cbs.update = fn
+	
+	# internally called, executes the update fn when any request completes
+	updated: (data, name, succeeded) ->
+		@count_complete++
+
+		if succeeded
+			@count_success++
+		else
+			@count_error++
+
+		@results.push(data)
+		if @cbs.update
+			@cbs.update(data, @status(), succeeded)
+
+	
+	# takes a single function
+	# fn is executed when any request fails.
+	# fn recieves the jqXHR object
+	error: (fn) ->
+		@cbs.error = fn
+	
+	# internally called, executes the error fn when any request fails
+	errored: (xhr, name) ->
+		if @cbs.error
+			@cbs.error(xhr, name)
+
+
+	# returns the current complete+total counts
+	status: (type = 'complete') ->
+		switch type
+			when 'success' then c = @count_success
+			when 'error' then c = @count_error
+			else c = @count_complete
+
+		return [c, @count_total]
+
+	# returns simple text based on status (english, useless)
+	status_text: (type = 'complete') ->
+		switch @status_percentage(type)
+			when 0   then return "Not started"
+			when 100 then return "Complete"
+			else          return "In Progress"
+
+	# returns a percentage of requests completed, with optional rounding
+	status_percentage: (type = 'complete', rounding = 0, sf = false) ->
+		if @count_total + @count_complete == 0
+			return 0
+		
+		if sf == false then sf = rounding
+		
+		vals = @status(type)
+		rounding = Math.pow(10, rounding)
+		perc     = (Math.round(100 * rounding * vals[0] / vals[1]) / rounding).toString()
+
+		if sf > 0
+			while (p = perc.indexOf('.')) < 0 then perc += '.'
+			while perc.length <= (p + sf) then perc += '0'
+
+		return perc
+
+	# adds a jqXHR object to the queue. should be trivial to alter other libs (superagent, moo, etc.) to fit.
+	add: (jq_xhr) ->
+		@count_total++
+
+		self = @
+		jq_xhr.complete((xhr, name) -> self.completed(xhr, name))
+		jq_xhr.success((data, name) -> self.updated(data, name, true); self.succeeded(data, name))
+		jq_xhr.fail((xhr, name) -> self.updated(xhr.responseText, name, false); self.errored(xhr, name))
+
+this.RequestQueue = RequestQueue
---
+++ queue.js
@@ -0,0 +1,135 @@
+(function() {
+  var RequestQueue;
+  RequestQueue = (function() {
+    function RequestQueue() {
+      this.cbs = {
+        complete: false,
+        success: false,
+        update: false,
+        error: false
+      };
+      this.count_total = 0;
+      this.count_complete = 0;
+      this.count_error = 0;
+      this.count_success = 0;
+      this.results = [];
+    }
+    RequestQueue.prototype.success = function(fn) {
+      return this.cbs.success = fn;
+    };
+    RequestQueue.prototype.succeeded = function(data, name) {
+      if (this.count_total === this.count_success && this.cbs.success) {
+        return this.cbs.success(this.results);
+      }
+    };
+    RequestQueue.prototype.complete = function(fn) {
+      return this.cbs.complete = fn;
+    };
+    RequestQueue.prototype.completed = function(data, name) {
+      if (this.count_total === this.count_complete && this.cbs.complete) {
+        return this.cbs.complete(this.results);
+      }
+    };
+    RequestQueue.prototype.update = function(fn) {
+      return this.cbs.update = fn;
+    };
+    RequestQueue.prototype.updated = function(data, name, succeeded) {
+      this.count_complete++;
+      if (succeeded) {
+        this.count_success++;
+      } else {
+        this.count_error++;
+      }
+      this.results.push(data);
+      if (this.cbs.update) {
+        return this.cbs.update(data, this.status(), succeeded);
+      }
+    };
+    RequestQueue.prototype.error = function(fn) {
+      return this.cbs.error = fn;
+    };
+    RequestQueue.prototype.errored = function(xhr, name) {
+      if (this.cbs.error) {
+        return this.cbs.error(xhr, name);
+      }
+    };
+    RequestQueue.prototype.status = function(type) {
+      var c;
+      if (type == null) {
+        type = 'complete';
+      }
+      switch (type) {
+        case 'success':
+          c = this.count_success;
+          break;
+        case 'error':
+          c = this.count_error;
+          break;
+        default:
+          c = this.count_complete;
+      }
+      return [c, this.count_total];
+    };
+    RequestQueue.prototype.status_text = function(type) {
+      if (type == null) {
+        type = 'complete';
+      }
+      switch (this.status_percentage(type)) {
+        case 0:
+          return "Not started";
+        case 100:
+          return "Complete";
+        default:
+          return "In Progress";
+      }
+    };
+    RequestQueue.prototype.status_percentage = function(type, rounding, sf) {
+      var p, perc, vals;
+      if (type == null) {
+        type = 'complete';
+      }
+      if (rounding == null) {
+        rounding = 0;
+      }
+      if (sf == null) {
+        sf = false;
+      }
+      if (this.count_total + this.count_complete === 0) {
+        return 0;
+      }
+      if (sf === false) {
+        sf = rounding;
+      }
+      vals = this.status(type);
+      rounding = Math.pow(10, rounding);
+      perc = (Math.round(100 * rounding * vals[0] / vals[1]) / rounding).toString();
+      if (sf > 0) {
+        while ((p = perc.indexOf('.')) < 0) {
+          perc += '.';
+        }
+        while (perc.length <= (p + sf)) {
+          perc += '0';
+        }
+      }
+      return perc;
+    };
+    RequestQueue.prototype.add = function(jq_xhr) {
+      var self;
+      this.count_total++;
+      self = this;
+      jq_xhr.complete(function(xhr, name) {
+        return self.completed(xhr, name);
+      });
+      jq_xhr.success(function(data, name) {
+        self.updated(data, name, true);
+        return self.succeeded(data, name);
+      });
+      return jq_xhr.fail(function(xhr, name) {
+        self.updated(xhr.responseText, name, false);
+        return self.errored(xhr, name);
+      });
+    };
+    return RequestQueue;
+  })();
+  this.RequestQueue = RequestQueue;
+}).call(this);
